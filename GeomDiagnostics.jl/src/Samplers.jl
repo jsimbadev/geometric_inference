4bda4415599ae8c3503feafcc6d9805a5644e6b8
Jordan Simbananiye <jgs2g12@gmail.com>
2026-02-27 11:45:25 +0000

    feat: fix some bad syntax for default args. Complete code paths for hnsw nn. Add some more tests for some code paths.

--- GeomDiagnostics.jl/src/Samplers.jl
+++ GeomDiagnostics.jl/src/Samplers.jl
@@ -44,8 +44,8 @@ end
 
 # TODO naming here 'data' is not the best... think over eventually
 # Do I want convernience methods to go from full fat NGPCA object in memory?
-function construct_ngpcacf_from_data(data::Matrix{Float64}, backend::NNBackend)
-    _construct_cf(data, backend)
+function construct_ngpcacf_from_data(data::AbstractMatrix{<:Real}, backend::NNBackend)
+    _construct_cf(Matrix{Float64}(data), backend)
 end
 
 function _construct_cf(data::Matrix{Float64}, backend::UseBallTree)
@@ -54,20 +54,25 @@ function _construct_cf(data::Matrix{Float64}, backend::UseBallTree)
 end
 
 
-function construct_cf(data::Matrix{Float64}, backend::UseHNSW)
-    nn = BallTreeNN(HierarchicalNSW(data; metric=backend.metric))
+function _construct_cf(data::Matrix{Float64}, backend::UseHNSW)
+    #  https://github.com/JuliaNeighbors/HNSW.jl
+    # TODO Making a copy here, probably dont want to do this eventually 
+    points = [Vector(data[:, i]) for i in axes(data, 2)]
+    hnsw_index = HierarchicalNSW(points; metric=backend.metric)
+    add_to_graph!(hnsw_index)
+    nn = HNSWNN(hnsw_index)
     NGPCACF{typeof(nn)}(nn)
 end
 
 
 # https://github.com/JuliaIO/JSON.jl/blob/f4fbb5a429a21b422c88883981c34e29d22b887e/src/parse.jl#L33
-function construct_ngpcacf_from_disk(ngpca_file::String, backend::NNBackend)
+function construct_ngpcacf_from_disk(ngpca_file::AbstractString, backend::NNBackend)
     units = NGPCAJson.read_ngpca_units(ngpca_file)
-    array_based_units = array_of_units_to_array_based(units)
+    array_based_units = NGPCAJson.array_of_units_to_array_based(units)
     construct_ngpcacf_from_data(array_based_units.centers, backend)
 end
 
-struct LocalCovarianceRWM{CF<: Atlas}
+struct LocalCovarianceRWM{CF<:AbstractCovarianceField}
     field::CF
 end
 
@@ -76,11 +81,11 @@ function get_knn(q::AbstractVector, cf::AbstractCovarianceField, k::Int=1)
     get_knn(q, cf.nn, k)
 end
 
-function get_knn(q::AbstractVector, nn::HNSWNN, k=Int=1)
+function get_knn(q::AbstractVector, nn::HNSWNN, k::Int=1)
     knn_search(nn.index, q, k)
 end
 
-function get_knn(q::AbstractVector, nn::BallTreeNN, k=Int=1)
+function get_knn(q::AbstractVector, nn::BallTreeNN, k::Int=1)
     knn(nn.index, q, k)
 end
 
--- GeomDiagnostics.jl/test/test_covariance_field.jl
+++ GeomDiagnostics.jl/test/test_covariance_field.jl
@@ -1,5 +1,6 @@
 
 using NearestNeighbors
+using JSON
 using GeomDiagnostics.Samplers
 import GeomDiagnostics.NGPCAJson: NGPCAUnitDO
 
@@ -23,15 +24,61 @@ test_covriance_data = NGPCAUnitDO(
     epsilons
 )
 
-covariance_field = Samplers.construct_ngpcacf_from_data(centers, Samplers.UseBallTree())
 
 @testset "Can instantiate NGPCA Covariance field given a matrix of centroid columns" begin
+    covariance_field = Samplers.construct_ngpcacf_from_data(centers, Samplers.UseBallTree())
     @test covariance_field isa Samplers.AbstractCovarianceField
+end
+
+@testset "Can instantiate NGPCA Covariance field with HNSW backend" begin
+    covariance_field_hnsw = Samplers.construct_ngpcacf_from_data(centers, Samplers.UseHNSW(Euclidean()))
+    @test covariance_field_hnsw isa Samplers.AbstractCovarianceField
+
+    idx, dist = Samplers.get_knn(Vector([1.1, 2.2]), covariance_field_hnsw, 1)
+    @test idx == [1]
+end
+
+@testset "Can instantiate NGPCA Covariance field from disk JSON" begin
+    tmp_json = tempname() * ".json"
+    units_obj = Dict(
+        "units" => [
+            Dict(
+                "m" => 2,
+                "center" => [1.0, 2.0],
+                "weight" => [[1.0, 0.0], [0.0, 1.0]],
+                "eigenvalue" => [1.0, 1.0],
+                "sigma_sqr" => 1.0,
+                "activity" => 1.0,
+                "alpha" => 0.1,
+                "epsilon" => 0.1
+            ),
+            Dict(
+                "m" => 2,
+                "center" => [3.0, 4.0],
+                "weight" => [[1.0, 0.0], [0.0, 1.0]],
+                "eigenvalue" => [1.0, 1.0],
+                "sigma_sqr" => 1.0,
+                "activity" => 1.0,
+                "alpha" => 0.1,
+                "epsilon" => 0.1
+            )
+        ]
+    )
+    write(tmp_json, JSON.json(units_obj))
+
+    covariance_field_disk = Samplers.construct_ngpcacf_from_disk(tmp_json, Samplers.UseBallTree())
+    @test covariance_field_disk isa Samplers.AbstractCovarianceField
+    idx, dist = Samplers.get_knn(Vector([3.1, 4.2]), covariance_field_disk, 1)
+    @test idx == [2]
+
+    rm(tmp_json; force=true)
 end
 
 @testset "Covariance field provides nearest neighbor" begin
     query1 = Vector([1.1, 2.2])
     query2 = Vector([3.1, 4.2])
+
+    covariance_field = Samplers.construct_ngpcacf_from_data(centers, Samplers.UseBallTree())
     
     idx1, dist1 = Samplers.get_knn(query1, covariance_field, 1)
     idx2, dist2 = Samplers.get_knn(query2, covariance_field, 1)
